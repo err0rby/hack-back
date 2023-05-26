cdd51e7cb61202529858193b921ee99e371e4c11
errorby <mdidayev@inbox.ru>
2023-05-26 09:56:47 +0300

    Создал бэк

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const start = async() => {
+    try {
+        await mongoose.connect(process.env.MONGOS);
+        console.log('Base');
+    } catch (error) {
+        console.log('Ошибка базы')
+    }
+}
+
+app.listen(process.env.PORT, () => {
+    console.log('Поднялся')
+});
+
+start();
